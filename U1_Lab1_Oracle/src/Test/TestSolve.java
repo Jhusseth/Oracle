9b2bc92f846bbd364706b7d84036be823ba64f18
jhusseth <jhusseth.sanchez@correo.icesi.edu.co>
2019-02-20 22:14:31 -0500

    diagramof class finished and begins diagrama of interfaz

--- U1_Lab1_Oracle/src/Test/TestSolve.java
+++ U1_Lab1_Oracle/src/Test/TestSolve.java
@@ -1,6 +1,5 @@
 package Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import model.Solve;
 
 import  org.junit.Test ;
@@ -10,13 +9,25 @@ public class TestSolve {
 	public Solve s;
 	
 	public void stage(){
-		s= new Solve(new double[]{1,2,1});	
+		s= new Solve();	
 	}
+	
 	
 	@Test
 	public void sceneSolve(){
 		stage();
+		double[] d  =  new double[]{1,2,1};
+		s = new Solve(d);
+		s.finsRoot();
 		
+		int c = 0;
+		
+		for(int i =0; i<s.realRoots.length;i++){
+			if(s.realRoots[i]!=0){
+				c++;
+			}
+		}
+		assertEquals(2, c);
 	}
 	
 	@Test
@@ -28,7 +39,6 @@ public class TestSolve {
 	@Test
 	public void sceneTwo(){
 		stage();
-		s.finsRoot();
 		
 	}
 	
